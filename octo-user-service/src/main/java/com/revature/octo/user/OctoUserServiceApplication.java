e973d5c14973e6e797a668aeb8f81b5747402c52
Benjamin Rogers <brogers0101@gmail.com>
2017-11-20 23:48:01 -0500

    docker test service

---
+++ octo-user-service/src/main/java/com/revature/octo/user/OctoUserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.revature.octo.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+//@EnableEurekaClient
+@SpringBootApplication
+public class OctoUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctoUserServiceApplication.class, args);
+	}
+}
